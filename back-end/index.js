6806b35c34bd37d4d2ff70012284dc679b8ceed6
manpreetsidhu31 <manusid2001@gmail.com>
2023-05-19 14:33:53 +0530

    updated link

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const PORT = 8080;
+const PORT = "https://ecommerce-mini-project.vercel.app/" || 8080;
 const app  = express();
 const cors = require('cors');
 const json = require('body-parser').json;
